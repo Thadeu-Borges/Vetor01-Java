bd885be4bab3fcc48785d2161f6293e95878dc24
Thadeu <123416610+Thadeu-Borges@users.noreply.github.com>
2023-02-19 04:15:59 -0300

    Java

---
+++ src/vetor01/Vetor01.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vetor01;
+
+/**
+ *
+ * @author AAAA
+ */
+public class Vetor01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic h
+        int n[] = {3,2,8,7,5,4};
+        System.out.println("Total de casas de N " + n.length);
+        for (int c=0; c<=5; c++) {
+            System.out.println( "Na posição " + c + " tem o valor " + n[c]);
+            
+        }
+        
+    }
+    
+}
